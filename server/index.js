19257d90059577610e2649e21211b5237741a12f
chethanredd <gschethan98@gmail.com>
2024-11-11 15:39:06 +0530

    logging in

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from "./routes/auth.js";
 import { register } from './controllers/auth.js';
 
 /*Configurations*/
@@ -36,8 +37,11 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+/*ROUTES WITH FILES */
 app.post("auth/register", upload.single("picture"), register);
 
+/*ROUTES*/
+app.use("/auth", authRoutes);
 /*ryHUOBX8crCBTCVc */
 /*Mongo Setup */
 const PORT = process.env.PORT || 6001;
