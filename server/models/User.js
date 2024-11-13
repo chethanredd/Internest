df0028ff5a6812f2408037cc290bc63fc9096399
chethanredd <gschethan98@gmail.com>
2024-11-13 12:45:57 +0530

    new

--- server/models/User.js
+++ server/models/User.js
@@ -1,45 +1,45 @@
 import mongoose from "mongoose";
 
-const Userschema = new mongoose.Schema(
-    {
-        FirstName: {
-            type: String,
-            required: true,
-            min: 2,
-            max: 50,
-        },
-        Lastname:  {
-            type: String,
-            required: true,
-            min: 2,
-            max: 50,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            max: 50,
-        },
-        password: {
-            type: String,
-            required: true,
-            min: 8,
-        },
-        picturePath:{
-            type: String,
-            default: "",
-        },
-        friends: {
-            type: Array,
-            default: []
-        },
-        location: String,
-        occpation: String,
-        viewedProfile: Number,
-        impressions: Number,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
     },
-    {timestamps: true}
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model("User", Userschema);
+const User = mongoose.model("User", UserSchema);
 export default User;
